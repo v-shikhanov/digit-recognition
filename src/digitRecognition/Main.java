1fb6a82ac21ea8ae8bcd56bddd5758ef07fd27ac
Vladislav <v.shichanov@gmail.com>
2018-11-15 18:19:48 +0300

    Javadoc comments are fixed.

--- src/digitRecognition/Main.java
+++ src/digitRecognition/Main.java
@@ -1,6 +1,24 @@
+/**
+ *
+ * @file
+ * @brief Here is Main of project Digit recognition. From here called  methods
+ * to complete task
+ *
+ *
+ * @authors Vladislav Shikhanov
+ *
+ *		  http://move-llc.ru
+ ************************************************************************************/
+
 package digitRecognition;
 
 public class Main {
+
+    /********************************************************************************
+     *
+     * main Method get path to images, read it, sort, check etc.
+     *
+     *******************************************************************************/
     public static void main(String[] args) {
         ReadIDX readIDX = new ReadIDX();
         ErrorFunction errorFunction = new ErrorFunction();
